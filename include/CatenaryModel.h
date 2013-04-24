0176f4551d0516a4a38769a6e4e43f742c380065
Corey M Bryant <corey.m.bryant@gmail.com>
2013-04-24 12:02:11 -0500

    Restructer includes and added Driver class

---
+++ include/CatenaryModel.h
@@ -0,0 +1,82 @@
+
+
+#ifndef CATENARY_MODEL_H
+#define CATENARY_MODEL_H
+
+#include "PhysicsModel.h"
+
+namespace AGNOS
+{
+
+  template<ParameterDataType, PhysicsDataType>
+  class CatenaryModel : public PhysicsModel
+  {
+
+  public:
+    CatenaryModel( );
+    ~CatenaryModel( );
+  
+  private:
+    double m_xMin;
+    double m_xMax;
+    double m_T;
+  };
+
+
+
+
+  // NOTE: function definitions actually depend on xMin=0 and xMax=1
+  //        and only coefficients for 1dof finite element solution are returned
+  template<ParameterDataType, PhysicsDataType>
+  CatenaryModel::CatenaryModel( )
+    : m_xMin = 0., m_xMax = 1., m_T = -10.;
+  {
+    return;
+  }
+
+  template<ParameterDataType, PhysicsDataType>
+    void CatenaryModel::solvePrimal( 
+        ParameterDataType& parameterValue  
+        )
+    {
+      m_primalSolution =  m_T / (8. * parameterValue) ;
+      return;
+    }
+
+  template<ParameterDataType, PhysicsDataType>
+    void CatenaryModel::solveAdjoint( 
+        ParameterDataType& parameterValue,  
+        PhysicsDataType& primalSolution    
+        )
+    {
+      m_adjointSolution = 1.0 / (4. * parameterValue) ;
+      return ;
+    }
+
+  template<ParameterDataType, PhysicsDataType>
+    double CatenaryModel::evaluateQoi( 
+        ParameterDataType& parameterValue,
+        PhysicsDataType& primalSolution    
+        )
+    {
+      // QoI is just primal at x=1/2 so its just coefficient value
+      return m_primalSolution ; 
+    }
+
+  template<ParameterDataType, PhysicsDataType>
+    double CatenaryModel::estimateError( 
+        ParameterDataType& parameterValue,  
+        PhysicsDataType& primalSolution,   
+        PhysicsDataType& adjointSolution  
+        )
+    {
+      // in this case the FE solution interpolates at x=1/2 so the QoI is
+      // evaluated exactly
+      return 0;
+    }
+
+
+
+}
+
+#endif // CATENARY_MODEL_H
