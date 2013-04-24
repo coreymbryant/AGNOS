a8c0aea0c425a7ff7c2b27f0f0fec24713a12949
Corey M Bryant <corey.m.bryant@gmail.com>
2013-04-24 18:44:33 -0500

    Working autotools
    Code doesn't compile though

--- include/CatenaryModel.h
+++ include/CatenaryModel.h
@@ -3,21 +3,38 @@
 #ifndef CATENARY_MODEL_H
 #define CATENARY_MODEL_H
 
-#define ParameterDataType double
-#define PhysicsDataType double
-
 #include "PhysicsModel.h"
 
 namespace AGNOS
 {
 
-  template<ParameterDataType, PhysicsDataType>
-  class CatenaryModel : public PhysicsModel
+  template<class T_S, class T_P>
+  class CatenaryModel : public PhysicsModel<T_S,T_P>
   {
 
   public:
     CatenaryModel( );
     ~CatenaryModel( );
+
+    virtual void solvePrimal( 
+        T_S& parameterValue  
+        );
+
+    virtual void solveAdjoint( 
+        T_S& parameterValue,  
+        T_P& primalSolution    
+        );
+
+    virtual double evaluateQoi( 
+        T_S& parameterValue,
+        T_P& primalSolution    
+        );
+
+    virtual double estimateError( 
+        T_S& parameterValue,  
+        T_P& primalSolution,   
+        T_P& adjointSolution  
+        );
   
   private:
     double m_T;
@@ -28,47 +45,48 @@ namespace AGNOS
 
   // NOTE: function definitions actually depend on xMin=0 and xMax=1
   //        and only coefficients for 1dof finite element solution are returned
-  template<ParameterDataType, PhysicsDataType>
-  CatenaryModel::CatenaryModel( )
-    :m_T = -10.;
+  template<class T_S, class T_P>
+  CatenaryModel<T_S,T_P>::CatenaryModel( )
+    :m_T(-10.0)
   {
     return;
   }
 
-  template<ParameterDataType, PhysicsDataType>
-    void CatenaryModel::solvePrimal( 
-        ParameterDataType& parameterValue  
+  template<class T_S, class T_P>
+    void CatenaryModel<T_S,T_P>::solvePrimal( 
+        T_S& parameterValue  
         )
     {
-      m_primalSolution =  m_T / (8. * parameterValue) ;
+      //m_primalSolution =  m_T / (8. * parameterValue) ;
       return;
     }
 
-  template<ParameterDataType, PhysicsDataType>
-    void CatenaryModel::solveAdjoint( 
-        ParameterDataType& parameterValue,  
-        PhysicsDataType& primalSolution    
+  template<class T_S, class T_P>
+    void CatenaryModel<T_S,T_P>::solveAdjoint( 
+        T_S& parameterValue,  
+        T_P& primalSolution    
         )
     {
-      m_adjointSolution = 1.0 / (4. * parameterValue) ;
+      //m_adjointSolution = 1.0 / (4. * parameterValue) ;
       return ;
     }
 
-  template<ParameterDataType, PhysicsDataType>
-    double CatenaryModel::evaluateQoi( 
-        ParameterDataType& parameterValue,
-        PhysicsDataType& primalSolution    
+  template<class T_S, class T_P>
+    double CatenaryModel<T_S,T_P>::evaluateQoi( 
+        T_S& parameterValue,
+        T_P& primalSolution    
         )
     {
       // QoI is just primal at x=1/2 so its just coefficient value
-      return m_primalSolution ; 
+      //return m_primalSolution ; 
+      return 0.;
     }
 
-  template<ParameterDataType, PhysicsDataType>
-    double CatenaryModel::estimateError( 
-        ParameterDataType& parameterValue,  
-        PhysicsDataType& primalSolution,   
-        PhysicsDataType& adjointSolution  
+  template<class T_S, class T_P>
+    double CatenaryModel<T_S,T_P>::estimateError( 
+        T_S& parameterValue,  
+        T_P& primalSolution,   
+        T_P& adjointSolution  
         )
     {
       // in this case the FE solution interpolates at x=1/2 so the QoI is
