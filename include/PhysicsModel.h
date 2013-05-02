afb36acbf0a0f24a5ac8f5413d51427a7c1f303d
Corey M Bryant <corey.m.bryant@gmail.com>
2013-05-02 16:07:52 -0500

    More progress
    Restructured the layout. Better routines for constructing surrogate.
    Need to implement computation of basis evaluations and index set.

--- include/PhysicsModel.h
+++ include/PhysicsModel.h
@@ -30,31 +30,31 @@ namespace AGNOS
       virtual T_P solvePrimal( 
           const T_S& parameterValue  
           ) = 0;
+      void setPrimalSolution( T_P& primalSolution );
 
       virtual T_P solveAdjoint( 
           const T_S& parameterValue,  
           const T_P& primalSolution    
           ) = 0;
-      virtual T_P solveAdjoint( const T_S& parameterValue ) ;
+      const T_P solveAdjoint( const T_S& parameterValue) ;
 
       virtual T_P evaluateQoi( 
           const T_S& parameterValue,
           const T_P& primalSolution    
           ) = 0;
-      virtual T_P evaluateQoi( const T_S& parameterValue ) ;
+      const T_P evaluateQoi( const T_S& parameterValue);
 
       virtual T_P estimateError( 
           const T_S& parameterValue,  
           const T_P& primalSolution,   
           const T_P& adjointSolution  
           ) = 0;
-      virtual T_P estimateError( const T_S& parameterValue ) ;
 
-      const T_P* getPrimalSolution( ) ;
-      const T_P* getAdjointSolution( ) ;
+      const T_P* getPrimalSolution( ) const;
+      const T_P* getAdjointSolution( ) const;
 
       // this may need a different type
-      const T_P* getErrorIndicators( ) ;
+      const T_P* getErrorIndicators( ) const;
 
       
     protected:
@@ -76,10 +76,15 @@ namespace AGNOS
   template<class T_S, class T_P>
   PhysicsModel<T_S,T_P>::PhysicsModel( )
   {
-    m_primalSolution  (NULL);
-    m_adjointSolution (NULL);
-    m_qoiValue        (NULL);
-    m_errorIndicators (NULL);
+    m_primalSolution  = new T_P;
+    m_adjointSolution = new T_P;
+    m_qoiValue        = new T_P;
+    m_errorIndicators = new T_P;
+
+    m_primalSolution  = NULL;
+    m_adjointSolution = NULL;
+    m_qoiValue        = NULL;
+    m_errorIndicators = NULL;
   }
 
 /********************************************//**
@@ -88,8 +93,10 @@ namespace AGNOS
   template<class T_S, class T_P>
   PhysicsModel<T_S,T_P>::~PhysicsModel( )
   {
+    delete [] m_currentParameter;
     delete [] m_primalSolution;
     delete [] m_adjointSolution;
+    delete [] m_qoiValue;
     delete [] m_errorIndicators;
   }
 
@@ -116,7 +123,7 @@ namespace AGNOS
  * \brief 
  ***********************************************/
   template<class T_S, class T_P>
-  const T_P PhysicsModel<T_S,T_P>::getErrorIndicators( ) const
+  const T_P* PhysicsModel<T_S,T_P>::getErrorIndicators( ) const
   {
     return m_errorIndicators;
   }
@@ -125,20 +132,44 @@ namespace AGNOS
  * \brief 
  ***********************************************/
   template<class T_S, class T_P>
-  T_P PhysicsModel<T_S,T_P>::solveAdjoint( const T_S& parameterValue ) ;
+  void T_P PhysicsModel<T_S,T_P>::setPrimalSolution( 
+      T_P& primalSolution )
+  {
+    *m_primalSolution = primalSolution ;
+
+    return 0;
+  }
 
 /********************************************//**
  * \brief 
  ***********************************************/
   template<class T_S, class T_P>
-  virtual T_P PhysicsModel<T_S,T_P>::evaluateQoi( const T_S& parameterValue ) ;
+  const T_P PhysicsModel<T_S,T_P>::solveAdjoint( 
+      const T_S& parameterValue )
+  {
+    //TODO we could have problems if sol is for different parameter value
+    if (m_primalSolution == NULL)
+      *m_primalSolution = solvePrimal( parameterValue ) ;
+    *m_adjointSolution = solveAdjoint( parameterValue, *m_primalSolution);
+
+    return *m_adjointSolution;
+  }
 
 /********************************************//**
  * \brief 
  ***********************************************/
   template<class T_S, class T_P>
-  virtual T_P PhysicsModel<T_S,T_P>::estimateError( const T_S& parameterValue ) ;
+  const T_P PhysicsModel<T_S,T_P>::evaluateQoi( 
+      const T_S& parameterValue )
+  {
+    if (m_primalSolution == NULL)
+      *m_primalSolution = solvePrimal( parameterValue ) ;
 
+    *m_qoiValue = evaluateQoi( parameterValue, *m_primalSolution );
+
+
+    return *m_qoiValue;
+  }
 }
 
 #endif //PHYSICS_MODEL_H
