3e923fe8af60afb5b89d82ddf23ca05a862a544b
Corey M Bryant <corey.m.bryant@gmail.com>
2013-06-04 16:29:36 -0500

    Moved example run to new examples dir

--- include/PhysicsViscousBurgers.h
+++ include/PhysicsViscousBurgers.h
@@ -2,6 +2,7 @@
 #ifndef PHYSICS_VISCOUS_BURGERS_H
 #define PHYSICS_VISCOUS_BURGERS_H
 
+#include "agnosDefines.h"
 #include "PhysicsModel.h"
 
 namespace AGNOS
@@ -16,22 +17,33 @@ namespace AGNOS
   class PhysicsViscousBurgers : public PhysicsModel<T_S,T_P>
   {
     public:
-      PhysicsViscousBurgers( ) ;
-      PhysicsViscousBurgers(int xMin, int xMin) ;
+      PhysicsViscousBurgers( GetPot& physicsInput ) ;
       
       ~PhysicsViscousBurgers( ) ;
 
     private:
-      int xMin;
-      int xMax;
-      double m_viscosity;
+      GetPot  m_input;
+      double     m_min;
+      double     m_max;
+      double  m_viscosity;
   };
 
 
   template<T_S, T_P>
-    PhysicsViscousBurgers::PhysicsViscousBurgers( )
-      : xMin = -10, xMax = 10
+    PhysicsViscousBurgers::PhysicsViscousBurgers(
+        GetPot& physicsInput
+        )
+      : m_input(physicsInput)
     {
+      std::cout << "\nReading Physics model data\n";
+
+      m_min = m_input("min",-10.);
+      m_max = m_input("max",10.);
+      m_viscosity = m_input("viscosity",1.);
+
+      std::cout << "\n Initializing libmesh model\n"
+      LibMeshInit init (argc, argv);
+
       return;
     }
 
@@ -46,6 +58,11 @@ namespace AGNOS
     void PhysicsViscousBurgers::solvePrimal(
         T_S& paramaterValue)
     {
+      // TODO   solve in libmesh the following problem
+      //        find u \in U = {h1 s.t. u(-10)=0, u(10)=1} \approx ?
+      //        a(u,v)=0 
+      //        a(u,v) = int_{-10}^{10} [ u(1-u) - 2 \mu u_x ] v_x dx
+      //        forall v \in V = {h1 s.t. v(-10)=v(10)=0}
       m_primalSolution = 1.0;
       return ;
     }
@@ -57,6 +74,11 @@ namespace AGNOS
         T_P& primalSolution
         )
     {
+      // TODO   solve in libmesh the following problem
+      //        find z \in V = {h1 s.t. z(-10)=0, z(10)=0}
+      //        a'(u;v,z)=0 
+      //        a'(u;v,z) = int_{-10}^{10} [ (1-2u)v - 2 \mu v_x ] z_x dx
+      //        forall v \in U 
       m_adjointSolution = -1.0;
       return ;
     }
