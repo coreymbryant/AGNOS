5ef1ca37d76ca03d3ea4142516d54677f3928a7a
Corey M Bryant <corey.m.bryant@gmail.com>
2013-07-16 16:34:00 -0500

    Base classes for libmesh physics
    Abstracted previous PhysicsCatenaryLibmesh into a base PhysicsLibmesh
    class; same thing for PhysicsQoi and PhysicsQoiDerivative. Added derived
    classes for Catenary and ViscousBurgers.

--- include/PhysicsQoi.h
+++ include/PhysicsQoi.h
@@ -22,7 +22,8 @@ namespace AGNOS
 
 
   /********************************************//**
-   * \brief Derive a libmesh qoi derivative class
+   * \brief Base class for defining quantities of interest, derived from libmesh
+   * qoi class 
    ***********************************************/
   template<class T_S>
   class PhysicsQoi : public libMesh::System::QOI
@@ -34,17 +35,20 @@ namespace AGNOS
           );
       ~PhysicsQoi( );
 
-      void qoi( const QoISet& qoi_indices );
+      virtual void qoi( const QoISet& qoi_indices ) = 0;
 
       void setParameterValues( const T_S& parameters );
 
-    private:
+    protected:
       T_S                       m_paramValues;
       libMesh::EquationSystems& m_es; 
       const std::string&        m_systemName;
 
   };
 
+  /********************************************//**
+   * \brief 
+   ***********************************************/
   template<class T_S>
     PhysicsQoi<T_S>::PhysicsQoi( 
         libMesh::EquationSystems& es, 
@@ -53,93 +57,16 @@ namespace AGNOS
     : m_es(es), m_systemName(systemName), m_paramValues( T_S(1) )
     { }
 
+  /********************************************//**
+   * \brief 
+   ***********************************************/
   template<class T_S>
     PhysicsQoi<T_S>::~PhysicsQoi( ){ }
 
-  template<class T_S>
-    void PhysicsQoi<T_S>::qoi( 
-        const QoISet& qoi_indices )
-    {
 
-      // reference to mesh
-      const libMesh::MeshBase& mesh = m_es.get_mesh();
-      const unsigned int dim = mesh.mesh_dimension();
-      libMesh::LinearImplicitSystem& system =
-        m_es.get_system<LinearImplicitSystem>("1D");
-
-      // dofs map
-      const libMesh::DofMap& dof_map = system.get_dof_map();
-      libMesh::FEType fe_type = dof_map.variable_type(0);
-
-      libMesh::AutoPtr<FEBase> fe(FEBase::build(dim, fe_type));
-
-      // quadrature
-      libMesh::QGauss qrule(dim,FOURTH);
-      fe->attach_quadrature_rule(&qrule);
-
-      // references to jacobians
-      const std::vector<libMesh::Real>& JxW = fe->get_JxW();
-      // shape functions at quadrature points
-      const std::vector<std::vector<libMesh::Real> >& phi = fe->get_phi();
-      const std::vector<std::vector<libMesh::RealGradient> >& dphi = fe->get_dphi();
-      const std::vector<Point > &q_point = fe->get_xyz();
-
-      // declare matrices
-      /* libMesh::DenseMatrix<libMesh::Number> Ke; */
-      /* libMesh::DenseVector<libMesh::Number> Fe; */
-      std::vector<libMesh::Number> &Q = system.qoi;
-      Q[0]=0;
-      std::vector<libMesh::dof_id_type> dof_indices;
-      
-      // loop over all elements
-      libMesh::MeshBase::const_element_iterator el     
-        = mesh.active_local_elements_begin();
-      const libMesh::MeshBase::const_element_iterator el_end 
-        = mesh.active_local_elements_end();
-
-      // Note that ++el is preferred to el++ when using loops with iterators
-      for( ; el != el_end; ++el)
-      {
-
-        // It is convenient to store a pointer to the current element
-        const libMesh::Elem* elem = *el;
-
-        // Get the degree of freedom indices for the current element. 
-        // These define where in the global matrix and right-hand-side this 
-        // element will contribute to.
-        dof_map.dof_indices(elem, dof_indices);
-
-        // Compute the element-specific data for the current element. This 
-        // involves computing the location of the quadrature points (q_point) 
-        // and the shape functions (phi, dphi) for the current element.
-        fe->reinit(elem);
-
-
-
-        // Now loop over quadrature points to handle numerical integration
-        for(unsigned int qp=0; qp<qrule.n_points(); qp++)
-        {
-           const Real x = q_point[qp](0);
-
-           // get solution value 
-           Number U = system.point_value(0,q_point[qp]);
-
-          // Now build the element matrix and right-hand-side using loops to
-          // integrate the test functions (i) against the trial functions (j).
-            Q[0] += JxW[qp] * U
-              * sqrt(100./(3.14159)) * std::exp( -100. * pow(x-0.5,2) );
-        }
-
-
-      }
-
-      // overwrite to exact point calculation
-      libMesh::Point point(0.5);
-      Q[0] = system.point_value(0,point);
-
-      return;
-    }
-
+  /********************************************//**
+   * \brief 
+   ***********************************************/
   template<class T_S> 
     void PhysicsQoi<T_S>::setParameterValues( const T_S& parameters) 
     { 
--- include/PhysicsQoiDerivative.h
+++ include/PhysicsQoiDerivative.h
@@ -22,7 +22,8 @@ namespace AGNOS
 
 
   /********************************************//**
-   * \brief Derive a libmesh qoi derivative class
+   * \brief Base QoiDerivative class for defining derivatives of Qoi for RHS of
+   * adjoint problem. Derived from libmesh QOIDerivative
    ***********************************************/
   template<class T_S>
   class PhysicsQoiDerivative : public libMesh::System::QOIDerivative
@@ -34,17 +35,20 @@ namespace AGNOS
           );
       ~PhysicsQoiDerivative( );
 
-      void qoi_derivative( const QoISet& qoi_indices );
+      virtual void qoi_derivative( const QoISet& qoi_indices ) = 0;
 
       void setParameterValues( const T_S& parameters );
 
-    private:
+    protected:
       T_S                       m_paramValues;
       libMesh::EquationSystems& m_es; 
       const std::string&        m_systemName;
 
   };
 
+  /********************************************//**
+   * \brief 
+   ***********************************************/
   template<class T_S>
     PhysicsQoiDerivative<T_S>::PhysicsQoiDerivative( 
         libMesh::EquationSystems& es, 
@@ -53,100 +57,16 @@ namespace AGNOS
     : m_es(es), m_systemName(systemName), m_paramValues( T_S(1) )
     { }
 
+  /********************************************//**
+   * \brief 
+   ***********************************************/
   template<class T_S>
     PhysicsQoiDerivative<T_S>::~PhysicsQoiDerivative( ){ }
 
-  template<class T_S>
-    void PhysicsQoiDerivative<T_S>::qoi_derivative( 
-        const QoISet& qoi_indices )
-    {
 
-      // reference to mesh
-      const libMesh::MeshBase& mesh = m_es.get_mesh();
-      const unsigned int dim = mesh.mesh_dimension();
-      libMesh::LinearImplicitSystem& system =
-        m_es.get_system<LinearImplicitSystem>("1D");
-
-      // dofs map
-      const libMesh::DofMap& dof_map = system.get_dof_map();
-      libMesh::FEType fe_type = dof_map.variable_type(0);
-
-      libMesh::AutoPtr<FEBase> fe(FEBase::build(dim, fe_type));
-
-      // quadrature
-      libMesh::QGauss qrule(dim,FOURTH);
-      fe->attach_quadrature_rule(&qrule);
-
-      // references to jacobians
-      const std::vector<libMesh::Real>& JxW = fe->get_JxW();
-      // shape functions at quadrature points
-      const std::vector<std::vector<libMesh::Real> >& phi = fe->get_phi();
-      const std::vector<std::vector<libMesh::RealGradient> >& dphi = fe->get_dphi();
-      const std::vector<Point > &q_point = fe->get_xyz();
-
-      // declare matrices
-      /* libMesh::DenseMatrix<libMesh::Number> Ke; */
-      /* libMesh::DenseVector<libMesh::Number> Fe; */
-      libMesh::DenseVector<libMesh::Number> Qe;
-      libMesh::NumericVector<libMesh::Number> &Q = system.get_adjoint_rhs();
-      std::vector<libMesh::dof_id_type> dof_indices;
-      
-      // loop over all elements
-      libMesh::MeshBase::const_element_iterator el     
-        = mesh.active_local_elements_begin();
-      const libMesh::MeshBase::const_element_iterator el_end 
-        = mesh.active_local_elements_end();
-
-      // Note that ++el is preferred to el++ when using loops with iterators
-      for( ; el != el_end; ++el)
-      {
-
-        // It is convenient to store a pointer to the current element
-        const libMesh::Elem* elem = *el;
-
-        // Get the degree of freedom indices for the current element. 
-        // These define where in the global matrix and right-hand-side this 
-        // element will contribute to.
-        dof_map.dof_indices(elem, dof_indices);
-
-        // Compute the element-specific data for the current element. This 
-        // involves computing the location of the quadrature points (q_point) 
-        // and the shape functions (phi, dphi) for the current element.
-        fe->reinit(elem);
-
-        // Store the number of local degrees of freedom contained in this element
-        const int n_dofs = dof_indices.size();
-
-        // We resize and zero out Ke and Fe (resize() also clears the matrix and
-        // vector). In this example, all elements in the mesh are EDGE3's, so 
-        // Ke will always be 3x3, and Fe will always be 3x1. If the mesh contained
-        // different element types, then the size of Ke and Fe would change.
-        /* Ke.resize(n_dofs, n_dofs); */
-        /* Fe.resize(n_dofs); */
-        Qe.resize(n_dofs);
-
-
-        // Now loop over quadrature points to handle numerical integration
-        for(unsigned int qp=0; qp<qrule.n_points(); qp++)
-        {
-           const Real x = q_point[qp](0);
-
-          // Now build the element matrix and right-hand-side using loops to
-          // integrate the test functions (i) against the trial functions (j).
-          for(unsigned int i=0; i<phi.size(); i++)
-          {
-            Qe(i) += JxW[qp]*phi[i][qp]
-              * sqrt(100./(3.14159)) * std::exp( -100. * pow(x-0.5,2) );
-          }
-        }
-
-        Q.add_vector(Qe, dof_indices);
-
-      }
-
-      return;
-    }
-
+  /********************************************//**
+   * \brief 
+   ***********************************************/
   template<class T_S> 
     void PhysicsQoiDerivative<T_S>::setParameterValues( const T_S& parameters) 
     { 
