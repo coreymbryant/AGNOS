3318cbbb38a62323dcc020a88e5be602161510bd
Corey M Bryant <corey.m.bryant@gmail.com>
2013-08-30 09:08:52 -0500

    Attempted update of print settings
    Still doesn't work ideally, but will have to change again when
    coefficient calcs are update.

--- test/ParallelCatenary.cpp
+++ test/ParallelCatenary.cpp
@@ -54,14 +54,6 @@ void initialize_mpi()
 
 BOOST_AUTO_TEST_SUITE(Catenary_tensorProduct)
 
-
-
-BOOST_AUTO_TEST_CASE(Catenary_N0)
-{
-
-  initialize_mpi();
-  comm = MPI_COMM_WORLD;
-  /* inputfile.set("physics/solutions","primal") ; */
   unsigned int dimension = 1;
   
   std::vector<Parameter*> myParameters(
@@ -71,6 +63,14 @@ BOOST_AUTO_TEST_CASE(Catenary_N0)
 
   PhysicsCatenary<T_S,T_P>* myPhysics = new PhysicsCatenary<T_S,T_P>(
       comm, inputfile );
+
+
+BOOST_AUTO_TEST_CASE(Catenary_N0)
+{
+
+  initialize_mpi();
+  comm = MPI_COMM_WORLD;
+  /* inputfile.set("physics/solutions","primal") ; */
 
   if (comm.rank()==0)
     BOOST_TEST_MESSAGE(" Testing Catenary with N=0");
@@ -93,176 +93,173 @@ BOOST_AUTO_TEST_CASE(Catenary_N0)
     BOOST_CHECK_CLOSE( myCoeff["primal"][0](0) , -10.0/16.0, 1e-9 );
   }
 
-  delete mySurrogate;
-  delete myPhysics;
-  myParameters.clear();
 
 }
 
-/* BOOST_AUTO_TEST_CASE(Catenary_N1) */
-/* { */
-/*   initialize_mpi(); */
-/*   comm = MPI_COMM_WORLD; */
-/*   /1* inputfile.set("physics/solutions","primal") ; *1/ */
+BOOST_AUTO_TEST_CASE(Catenary_N1)
+{
+  initialize_mpi();
+  comm = MPI_COMM_WORLD;
+  /* inputfile.set("physics/solutions","primal") ; */
 
-/*   if (comm.rank()==0) */
-/*     BOOST_TEST_MESSAGE(" Testing Catenary with N=1"); */
+  if (comm.rank()==0)
+    BOOST_TEST_MESSAGE(" Testing Catenary with N=1");
 
-/*   std::vector<unsigned int> myOrder(dimension,1); */
-/*   PseudoSpectralTensorProduct<T_S,T_P>* mySurrogate = */ 
-/*     new PseudoSpectralTensorProduct<T_S,T_P>( */
-/*         comm, */
-/*         myPhysics, */ 
-/*         myParameters, */ 
-/*         myOrder */  
-/*         ); */
+  std::vector<unsigned int> myOrder(dimension,1);
+  PseudoSpectralTensorProduct<T_S,T_P>* mySurrogate = 
+    new PseudoSpectralTensorProduct<T_S,T_P>(
+        comm,
+        myPhysics, 
+        myParameters, 
+        myOrder  
+        );
 
-/*   mySurrogate->build( ); */
-/*   std::map< std::string, std::vector<T_P> > myCoeff; */
+  mySurrogate->build( );
+  std::map< std::string, std::vector<T_P> > myCoeff;
 
-/*   myCoeff = mySurrogate->getCoefficients(); */
+  myCoeff = mySurrogate->getCoefficients();
 
-/*   if (comm.rank() == 0) */
-/*   { */
-/*     // 12/11 is the sum of u(\xi_j) */ 
-/*     BOOST_CHECK_CLOSE( */ 
-/*         myCoeff["primal"][0](0), */
-/*         -10.0/16.0 * (12./11.), 1e-9 ); */
+  if (comm.rank() == 0)
+  {
+    // 12/11 is the sum of u(\xi_j) 
+    BOOST_CHECK_CLOSE( 
+        myCoeff["primal"][0](0),
+        -10.0/16.0 * (12./11.), 1e-9 );
 
-/*     // -2 sqrt(3)/11 is correct contribution from poly evals */
-/*     BOOST_CHECK_CLOSE( */ 
-/*         myCoeff["primal"][1](0) , */ 
-/*         -10.0/16.0 * ( -2.0 * std::sqrt(3.0) / 11.0 ), */ 
-/*         1e-9 ); */
-/*   } */
+    // -2 sqrt(3)/11 is correct contribution from poly evals
+    BOOST_CHECK_CLOSE( 
+        myCoeff["primal"][1](0) , 
+        -10.0/16.0 * ( -2.0 * std::sqrt(3.0) / 11.0 ), 
+        1e-9 );
+  }
 
-/* } */
+}
 
-/* BOOST_AUTO_TEST_CASE(Catenary_N4) */
-/* { */
-/*   initialize_mpi(); */
-/*   comm = MPI_COMM_WORLD; */
-/*   /1* inputfile.set("physics/solutions","primal") ; *1/ */
+BOOST_AUTO_TEST_CASE(Catenary_N4)
+{
+  initialize_mpi();
+  comm = MPI_COMM_WORLD;
+  /* inputfile.set("physics/solutions","primal") ; */
 
-/*   if (comm.rank()==0) */
-/*     BOOST_TEST_MESSAGE(" Testing Catenary with N=4"); */
+  if (comm.rank()==0)
+    BOOST_TEST_MESSAGE(" Testing Catenary with N=4");
 
-/*   std::vector<unsigned int> myOrder(dimension,4); */
-/*   PseudoSpectralTensorProduct<T_S,T_P>* mySurrogate = */ 
-/*     new PseudoSpectralTensorProduct<T_S,T_P>( */
-/*         comm, */
-/*         myPhysics, */ 
-/*         myParameters, */ 
-/*         myOrder */  
-/*         ); */
+  std::vector<unsigned int> myOrder(dimension,4);
+  PseudoSpectralTensorProduct<T_S,T_P>* mySurrogate = 
+    new PseudoSpectralTensorProduct<T_S,T_P>(
+        comm,
+        myPhysics, 
+        myParameters, 
+        myOrder  
+        );
 
-/*   mySurrogate->build( ); */
-/*   std::map< std::string, std::vector<T_P> > myCoeff ; */
+  mySurrogate->build( );
+  std::map< std::string, std::vector<T_P> > myCoeff ;
 
-/*   myCoeff = mySurrogate->getCoefficients(); */
+  myCoeff = mySurrogate->getCoefficients();
 
-/*   if (comm.rank() == 0) */
-/*   { */
-/*     // Coefficients generated from pmpack for comparison */
-/*     BOOST_CHECK_CLOSE( */ 
-/*         myCoeff["primal"][0](0) , */ 
-/*         -6.866307761327950e-01, */
-/*         1e-9 ); */
-/*     std::cout << "myCoeff[0]=" << myCoeff["primal"][0](0) << std::endl; */
+  if (comm.rank() == 0)
+  {
+    // Coefficients generated from pmpack for comparison
+    BOOST_CHECK_CLOSE( 
+        myCoeff["primal"][0](0) , 
+        -6.866307761327950e-01,
+        1e-9 );
+    std::cout << "myCoeff[0]=" << myCoeff["primal"][0](0) << std::endl;
     
-/*     BOOST_CHECK_CLOSE( */ 
-/*         myCoeff["primal"][1](0) , */ 
-/*         2.134952711438086e-01, */
-/*         1e-9 ); */
-/*     std::cout << "myCoeff[1]=" << myCoeff["primal"][1](0) << std::endl; */
+    BOOST_CHECK_CLOSE( 
+        myCoeff["primal"][1](0) , 
+        2.134952711438086e-01,
+        1e-9 );
+    std::cout << "myCoeff[1]=" << myCoeff["primal"][1](0) << std::endl;
    
-/*     BOOST_CHECK_CLOSE( */ 
-/*         myCoeff["primal"][2](0) , */ 
-/*         -5.918708419582225e-02, */ 
-/*         1e-9 ); */
-/*     std::cout << "myCoeff[2]=" << myCoeff["primal"][2](0) << std::endl; */
+    BOOST_CHECK_CLOSE( 
+        myCoeff["primal"][2](0) , 
+        -5.918708419582225e-02, 
+        1e-9 );
+    std::cout << "myCoeff[2]=" << myCoeff["primal"][2](0) << std::endl;
 
-/*     BOOST_CHECK_CLOSE( */ 
-/*         myCoeff["primal"][3](0) , */ 
-/*         1.602406581398491e-02, */
-/*         1e-9 ); */
-/*     std::cout << "myCoeff[3]=" << myCoeff["primal"][3](0) << std::endl; */
+    BOOST_CHECK_CLOSE( 
+        myCoeff["primal"][3](0) , 
+        1.602406581398491e-02,
+        1e-9 );
+    std::cout << "myCoeff[3]=" << myCoeff["primal"][3](0) << std::endl;
 
-/*     BOOST_CHECK_CLOSE( */ 
-/*         myCoeff["primal"][4](0) , */ 
-/*         -4.037685060565489e-03, */
-/*         1e-9 ); */
-/*     std::cout << "myCoeff[4]=" << myCoeff["primal"][4](0) << std::endl; */
-/*   } */
+    BOOST_CHECK_CLOSE( 
+        myCoeff["primal"][4](0) , 
+        -4.037685060565489e-03,
+        1e-9 );
+    std::cout << "myCoeff[4]=" << myCoeff["primal"][4](0) << std::endl;
+  }
 
-/* } */
+}
 
-/* BOOST_AUTO_TEST_CASE(Catenary_mean) */
-/* { */
-/*   initialize_mpi(); */
-/*   comm = MPI_COMM_WORLD; */
-/*   /1* inputfile.set("physics/solutions","primal") ; *1/ */
+BOOST_AUTO_TEST_CASE(Catenary_mean)
+{
+  initialize_mpi();
+  comm = MPI_COMM_WORLD;
+  /* inputfile.set("physics/solutions","primal") ; */
 
-/*   if (comm.rank()==0) */
-/*     BOOST_TEST_MESSAGE(" Testing Catenary with N=4 for mean calculation"); */
+  if (comm.rank()==0)
+    BOOST_TEST_MESSAGE(" Testing Catenary with N=4 for mean calculation");
 
-/*   std::vector<unsigned int> myOrder(dimension,4); */
-/*   PseudoSpectralTensorProduct<T_S,T_P>* mySurrogate = */ 
-/*     new PseudoSpectralTensorProduct<T_S,T_P>( */
-/*         comm, */
-/*         myPhysics, */ 
-/*         myParameters, */ 
-/*         myOrder */  
-/*         ); */
+  std::vector<unsigned int> myOrder(dimension,4);
+  PseudoSpectralTensorProduct<T_S,T_P>* mySurrogate = 
+    new PseudoSpectralTensorProduct<T_S,T_P>(
+        comm,
+        myPhysics, 
+        myParameters, 
+        myOrder  
+        );
 
-/*   mySurrogate->build( ); */
-/*   std::map< std::string, T_P > myMean ; */
+  mySurrogate->build( );
+  std::map< std::string, T_P > myMean ;
 
-/*   myMean = mySurrogate->mean(); */
+  myMean = mySurrogate->mean();
 
-/*   // Coefficients generated from pmpack for comparison */
-/*   BOOST_CHECK_CLOSE( */ 
-/*       myMean["primal"](0) , */ 
-/*       -6.866307761327950e-01, */
-/*       1e-9 ); */
+  // Coefficients generated from pmpack for comparison
+  BOOST_CHECK_CLOSE( 
+      myMean["primal"](0) , 
+      -6.866307761327950e-01,
+      1e-9 );
     
 
-/* } */
+}
 
-/* BOOST_AUTO_TEST_CASE(Catenary_convergence) */
-/* { */
-/*   initialize_mpi(); */
-/*   comm = MPI_COMM_WORLD; */
-/*   /1* inputfile.set("physics/solutions","primal") ; *1/ */
+BOOST_AUTO_TEST_CASE(Catenary_convergence)
+{
+  initialize_mpi();
+  comm = MPI_COMM_WORLD;
+  /* inputfile.set("physics/solutions","primal") ; */
 
-/*   if (comm.rank()==0) */
-/*     BOOST_TEST_MESSAGE(" Testing Catenary convergence"); */
+  if (comm.rank()==0)
+    BOOST_TEST_MESSAGE(" Testing Catenary convergence");
 
-/*   std::vector<unsigned int> myOrder(dimension,0); */
-/*   PseudoSpectralTensorProduct<T_S,T_P>* mySurrogate = */ 
-/*     new PseudoSpectralTensorProduct<T_S,T_P>( */
-/*         comm, */
-/*         myPhysics, */ 
-/*         myParameters, */ 
-/*         myOrder */  
-/*         ); */
+  std::vector<unsigned int> myOrder(dimension,0);
+  PseudoSpectralTensorProduct<T_S,T_P>* mySurrogate = 
+    new PseudoSpectralTensorProduct<T_S,T_P>(
+        comm,
+        myPhysics, 
+        myParameters, 
+        myOrder  
+        );
 
-/*   T_S paramValue(dimension); */
-/*   paramValue(0) = 1.5; */
-/*   T_P testValue; */ 
+  T_S paramValue(dimension);
+  paramValue(0) = 1.5;
+  T_P testValue; 
 
-/*   unsigned int maxIter = 25; */
-/*   for (unsigned int iter=0; iter < maxIter-1; iter++) */
-/*   { */
-/*     mySurrogate->refine(); */
-/*     testValue = mySurrogate->evaluate( "primal", paramValue ) ; */
-/*   } */
+  unsigned int maxIter = 25;
+  for (unsigned int iter=0; iter < maxIter-1; iter++)
+  {
+    mySurrogate->refine();
+    testValue = mySurrogate->evaluate( "primal", paramValue ) ;
+  }
 
-/*   if (comm.rank() == 0) */
-/*     BOOST_CHECK_CLOSE(  testValue(0) , -10.0/(8.0 * paramValue(0) ), 1e-9 ); */
+  if (comm.rank() == 0)
+    BOOST_CHECK_CLOSE(  testValue(0) , -10.0/(8.0 * paramValue(0) ), 1e-9 );
 
 
-/* } */
+}
 
 
 BOOST_AUTO_TEST_SUITE_END()
