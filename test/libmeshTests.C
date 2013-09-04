cd9ad17bc9ea666ee8e7238fd17482196e179d92
Corey M Bryant <corey.m.bryant@gmail.com>
2013-09-03 23:00:33 -0500

    Fixed parallel issue
    Issue: MPI_Finalize wasn't called properly. Fix passes all tests now.

--- test/libmeshTests.C
+++ test/libmeshTests.C
@@ -8,19 +8,22 @@
 int main( int argc, char **argv)
 {
   MPI_Init(&argc,&argv);
-
   Communicator comm(MPI_COMM_WORLD);
-  MPI_Comm subComm;
-  int mpiSplit =  
-    MPI_Comm_split( MPI_COMM_WORLD, comm.rank(), 0, &subComm);
-  LibMeshInit libmesh_init(argc, argv, subComm);
 
-  CppUnit::TextUi::TestRunner runner;
-  CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
-  runner.addTest( registry.makeTest() );
-  runner.run();
+  {
+    MPI_Comm subComm;
+    int mpiSplit =  
+      MPI_Comm_split( MPI_COMM_WORLD, comm.rank(), 0, &subComm);
+    LibMeshInit libmesh_init(argc, argv, subComm);
 
+    CppUnit::TextUi::TestRunner runner;
+    CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
+    runner.addTest( registry.makeTest() );
+    runner.run();
+  }
+
+  int ierr;
   comm.barrier();
-
+  ierr = MPI_Finalize();
   return 0;
 }
