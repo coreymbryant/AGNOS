7df380411b56cbd32deedc83863b7ec2b436d364
Corey M Bryant <corey.m.bryant@gmail.com>
2014-03-27 17:45:49 -0500

    Convergence test for ChannelFlow
    - solves for nominal sol's at params = 1
    - constructs uniform surrogate over [0.9,1.1]
    - compares evaluation of uniform surrogate at params=1 to nominal sol's
    Closes #44

--- test/ChannelTest.C
+++ test/ChannelTest.C
@@ -17,6 +17,9 @@
 #include "spalart_allmaras.h"
 
 // local
+#include "agnosDefines.h"
+#include "Parameter.h"
+#include "PseudoSpectralTensorProduct.h"
 #include "PhysicsChannelFlow.h"
 
 // boost
@@ -24,6 +27,7 @@
 #include <boost/test/included/unit_test.hpp>
 #include <boost/test/floating_point_comparison.hpp>
 
+using namespace AGNOS;
 
 BOOST_AUTO_TEST_CASE( channel_constructor )
 {
@@ -38,11 +42,13 @@ BOOST_AUTO_TEST_CASE( channel_constructor )
   av[0] = name;
   
   // Initialize libmesh
+  MPI_Init(&ac,&av);
+  Communicator comm(MPI_COMM_WORLD);
   LibMeshInit init (ac, av);
 
   GetPot inputfile;
   inputfile = GetPot( );
-  AGNOS::PhysicsChannelFlow<T_S,T_P> flowSolver(
+  PhysicsChannelFlow<T_S,T_P> flowSolver(
       Communicator(MPI_COMM_NULL),inputfile
       );
 
@@ -73,13 +79,15 @@ BOOST_AUTO_TEST_CASE( channel_solve )
   av[0] = name;
   
   // Initialize libmesh
+  MPI_Init(&ac,&av);
+  Communicator comm(MPI_COMM_WORLD);
   LibMeshInit init (ac, av);
 
   GetPot inputfile;
   inputfile = GetPot( );
   inputfile.set("channel_input","flow.in") ;
-  AGNOS::PhysicsChannelFlow<T_S,T_P> flowSolver(
-      Communicator(MPI_COMM_NULL),inputfile
+  PhysicsChannelFlow<T_S,T_P> flowSolver(
+      comm,inputfile
       );
 
   // test setting parameter values
@@ -92,15 +100,115 @@ BOOST_AUTO_TEST_CASE( channel_solve )
     static_cast<spalartAllmarasTurbulenceModel*>(
         system->_turbModel.get() );
 
-  BOOST_CHECK_CLOSE( turb->_kap, params[0]*0.41 ,1e-9);
-  BOOST_CHECK_CLOSE( turb->_cb1 , params[1]*0.1355, 1e-9);
-  BOOST_CHECK_CLOSE( turb->_sig  , params[2]*2.0/3.0, 1e-9);
-  BOOST_CHECK_CLOSE( turb->_cb2    , params[3]*0.622, 1e-9);
-  BOOST_CHECK_CLOSE( turb->_cv1 , params[4]*7.1, 1e-9);
-  BOOST_CHECK_CLOSE( turb->_cw2 , params[5]*0.3, 1e-9);
-  BOOST_CHECK_CLOSE( turb->_cw3 , params[6]*2.0, 1e-9);
+  BOOST_REQUIRE_CLOSE( turb->_kap, params[0]*0.41 ,1e-9);
+  BOOST_REQUIRE_CLOSE( turb->_cb1 , params[1]*0.1355, 1e-9);
+  BOOST_REQUIRE_CLOSE( turb->_sig  , params[2]*2.0/3.0, 1e-9);
+  BOOST_REQUIRE_CLOSE( turb->_cb2    , params[3]*0.622, 1e-9);
+  BOOST_REQUIRE_CLOSE( turb->_cv1 , params[4]*7.1, 1e-9);
+  BOOST_REQUIRE_CLOSE( turb->_cw2 , params[5]*0.3, 1e-9);
+  BOOST_REQUIRE_CLOSE( turb->_cw3 , params[6]*2.0, 1e-9);
 
   // make sure nothing catastrophic happens if we try to solve
   flowSolver._solve() ;
+
+}
+
+BOOST_AUTO_TEST_CASE( channel_convergence )
+{
+  // Set dummy inputs for libmesh initialization
+  int ac=1;
+  char** av = new char* [ac];
+
+  char* name = new char[27];
+  strcpy(name, "dummy");
+  
+  av[0] = name;
+  
+  // Initialize libmesh
+  MPI_Init(&ac,&av);
+  Communicator comm(MPI_COMM_WORLD);
+  LibMeshInit init (ac, av);
+
+  // istantiate phyiscs model
+  GetPot inputfile;
+  inputfile = GetPot( );
+  inputfile.set("channel_input","flow.in") ;
+  std::shared_ptr<PhysicsLibmesh<T_S,T_P> > flowSolver ; 
+  flowSolver = std::shared_ptr<PhysicsChannelFlow<T_S,T_P> >(
+      new PhysicsChannelFlow<T_S,T_P> ( comm,inputfile )
+      );
+
+  // construct parameter object
+  unsigned int dimension = 7;
+  std::vector<std::shared_ptr<AGNOS::Parameter> > parameters;
+
+  parameters.reserve(dimension);
+  for(unsigned int i =0; i<dimension; i++)
+    parameters.push_back( 
+        std::shared_ptr<AGNOS::Parameter>(
+          new AGNOS::Parameter("CONSTANT",1.0, 1.0) )
+      ); 
+
+  // build the constantSurrogate model
+  std::vector<unsigned int> order(dimension,0);
+  std::set<std::string> computeSolutions ;
+  computeSolutions.insert("primal");
+  computeSolutions.insert("adjoint");
+  PseudoSpectralTensorProduct<T_S,T_P> constantSurrogate(
+        comm,
+        flowSolver,
+        parameters, 
+        order,
+        computeSolutions
+        );
+
+  constantSurrogate.build( );
+
+  // get the nominal solution we will compare to
+  std::vector<double> nominalParamValue(dimension,1.);
+  T_S paramValue(nominalParamValue);
+  T_P primalSol = constantSurrogate.evaluate( "primal", paramValue );
+  T_P adjointSol = constantSurrogate.evaluate( "adjoint", paramValue );
+
+
+  // update parameters to small range around nominal values 
+  parameters.clear();
+  parameters.reserve(dimension);
+  for(unsigned int i =0; i<dimension; i++)
+    parameters.push_back( 
+        std::shared_ptr<AGNOS::Parameter>(
+          new AGNOS::Parameter("UNIFORM",0.90, 1.10) )
+      ); 
+  
+  // build the uniformSurrogate model
+  /* std::vector<unsigned int> higherOrder(dimension,1); */
+  PseudoSpectralTensorProduct<T_S,T_P> uniformSurrogate(
+        comm,
+        flowSolver,
+        parameters, 
+        order  
+        );
+
+  T_P primalPred, adjointPred;
+
+  int iter=0, maxIter=2;
+  double tol = 1e-12;
+  do 
+  {
+    iter++;
+    uniformSurrogate.refine();
+    uniformSurrogate.build( );
+    primalPred = uniformSurrogate.evaluate( "primal", paramValue );
+    primalPred -= primalSol ;
+    adjointPred = uniformSurrogate.evaluate( "adjoint", paramValue );
+    adjointPred -= adjointSol ;
+  } while ( 
+       (iter<maxIter) && 
+       (primalPred.linfty_norm()>tol) &&
+       (adjointPred.linfty_norm()>tol) 
+       ) ;
+
+  BOOST_REQUIRE_SMALL( primalPred.linfty_norm(), tol  );
+  BOOST_REQUIRE_SMALL( adjointPred.linfty_norm(), tol  );
 
 }
