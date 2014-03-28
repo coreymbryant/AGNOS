1a50fdce5e88d1038cd6fdb1825ca196693235ba
Corey M Bryant <corey.m.bryant@gmail.com>
2014-03-27 20:05:57 -0500

    Moved index compare function to defines

--- include/agnosDefines.h
+++ include/agnosDefines.h
@@ -62,6 +62,27 @@ typedef libMesh::DenseMatrix<double> LocalMatrix;
     } } while(0)
 
 namespace AGNOS{
+  /* /** Comparison functor for index sets *1/ */
+  /* bool indexSetCompare( */ 
+  /*     const std::vector<unsigned int>& a, */ 
+  /*     const std::vector<unsigned int>& b */ 
+  /*     ) */
+  /* { */
+  /*   assert( a.size() == b.size() ) ; */
+  /*   bool aLessThanb = false; */
+
+  /*   for(unsigned int i=0; i<a.size(); i++) */
+  /*     if ( a[i] < b[i] ) */
+  /*     { */
+  /*       aLessThanb = true ; */
+  /*       break; */
+  /*     } */
+  /*     else if ( a[i] > b[i] ) */
+  /*       break; */
+
+  /*   return aLessThanb; */
+  /* } */
+
 }
 
 
