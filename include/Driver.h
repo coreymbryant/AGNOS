086ab9185ce25fd2a3ed3a3f15537316a126b523
Corey M Bryant <corey.m.bryant@gmail.com>
2014-03-27 20:05:57 -0500

    Moved index compare function to defines

--- include/Driver.h
+++ include/Driver.h
@@ -25,27 +25,6 @@
 namespace AGNOS
 {
 
-  /** Comparison functor for index sets */
-  bool indexSetCompare( 
-      const std::vector<unsigned int>& a, 
-      const std::vector<unsigned int>& b 
-      )
-  {
-    assert( a.size() == b.size() ) ;
-    bool aLessThanb = false;
-
-    for(unsigned int i=0; i<a.size(); i++)
-      if ( a[i] < b[i] )
-      {
-        aLessThanb = true ;
-        break;
-      }
-      else if ( a[i] > b[i] )
-        break;
-
-    return aLessThanb;
-  }
-
   /********************************************//**
    * \brief Base class for driving the construction of SurrogateModel objects
    ***********************************************/
