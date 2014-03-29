5108939012313ebe6bd630e34c8906b9922ba724
Corey M Bryant <corey.m.bryant@gmail.com>
2014-03-29 10:19:28 -0500

    Updating header files before renamining
    we need to do this in stages to make sure the file history remains
    intact.

--- include/QuadratureRule.h
+++ include/QuadratureRule.h
@@ -2,8 +2,8 @@
 #ifndef QUADRATURE_RULE_H
 #define QUADRATURE_RULE_H
 
-#include "sandia_rules.hpp"
 #include "Parameter.h"
+#include "sandia_rules.hpp"
 
 namespace AGNOS
 {
@@ -30,67 +30,6 @@ namespace AGNOS
       double** m_quadPoints;
       double* m_quadWeights;
   };
-
-/********************************************//**
- * \brief 
- * 
- ***********************************************/
-  QuadratureRule::QuadratureRule( )
-  {
-    // Do nothing, this needs to be defined for derived classes
-  }
-
-/********************************************//**
- * \brief 
- * 
- ***********************************************/
-  QuadratureRule::~QuadratureRule( )
-  {
-    for (unsigned int i=0; i< m_nQuadPoints; i++)
-      delete [] m_quadPoints[i] ;
-
-    delete [] m_quadPoints;
-    delete [] m_quadWeights;
-  }
-
-/********************************************//**
- * \brief 
- *
- * 
- ***********************************************/
-  unsigned int QuadratureRule::getNQuadPoints( ) const
-  {
-    return m_nQuadPoints;
-  }
-
-/********************************************//**
- * \brief 
- *
- * 
- ***********************************************/
-  unsigned int QuadratureRule::getDimension( ) const
-  {
-    return m_dimension;
-  }
-
-/********************************************//**
- * \brief 
- *
- * 
- ***********************************************/
-  double** QuadratureRule::getQuadPoints( ) const
-  {
-    return m_quadPoints;
-  }
-/********************************************//**
- * \brief 
- *
- * 
- ***********************************************/
-  double* QuadratureRule::getQuadWeights( ) const
-  {
-    return m_quadWeights;
-  }
 
 
 }
