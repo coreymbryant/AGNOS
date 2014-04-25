28b5f3386e1fa4798095979cdcc10820c5291844
Corey M Bryant <corey.m.bryant@gmail.com>
2014-04-25 15:52:42 -0500

    Added convergence to GrinsTest

--- test/GrinsTest.C
+++ test/GrinsTest.C
@@ -22,7 +22,7 @@
 #include "grins/inc_navier_stokes.h"
 
 // boost
-#define BOOST_TEST_MODULE channelTest
+#define BOOST_TEST_MODULE grinsTest
 #include <boost/test/included/unit_test.hpp>
 #include <boost/test/floating_point_comparison.hpp>
 
@@ -113,150 +113,163 @@ BOOST_AUTO_TEST_CASE( grins_solve )
 
 }
 
-/* BOOST_AUTO_TEST_CASE( channel_convergence ) */
-/* { */
-/*   // Set dummy inputs for libmesh initialization */
-/*   int ac=1; */
-/*   char** av = new char* [ac]; */
+BOOST_AUTO_TEST_CASE( grins_convergence )
+{
+  // Set dummy inputs for libmesh initialization
+  int ac=1;
+  char** av = new char* [ac];
 
-/*   char* name = new char[27]; */
-/*   strcpy(name, "dummy"); */
+  char* name = new char[27];
+  strcpy(name, "dummy");
   
-/*   av[0] = name; */
+  av[0] = name;
   
-/*   // Initialize libmesh */
-/*   MPI_Init(&ac,&av); */
-/*   Communicator comm(MPI_COMM_WORLD); */
-/*   PETSC_COMM_WORLD = MPI_COMM_WORLD ; */
-/*   int ierr = PetscInitialize(&ac, const_cast<char***>(&av),NULL,NULL); */
-/*   LibMeshInit init (ac, av); */
+  // Initialize libmesh
+  MPI_Init(&ac,&av);
+  Communicator comm(MPI_COMM_WORLD);
+  PETSC_COMM_WORLD = MPI_COMM_WORLD ;
+  int ierr = PetscInitialize(&ac, const_cast<char***>(&av),NULL,NULL);
+  LibMeshInit init (ac, av);
 
-/*   // istantiate phyiscs model */
-/*   GetPot inputfile; */
-/*   inputfile = GetPot( ); */
-/*   inputfile.set("channel_input","flow.in") ; */
-/*   std::shared_ptr<PhysicsLibmesh<T_S,T_P> > grinsSolver ; */ 
-/*   grinsSolver = std::shared_ptr<PhysicsGrins<T_S,T_P> >( */
-/*       new PhysicsGrins<T_S,T_P> ( comm,inputfile ) */
-/*       ); */
+  // istantiate phyiscs model
+  GetPot inputfile;
+  inputfile = GetPot( );
+  inputfile.set("physics/grins_input","./grins.in") ;
+  /* inputfile.set("physics/IncompressibleNavierStokes/mu","$(0)") ; */
+  /* inputfile.set( */
+  /*     "physics/IncompressibleNavierStokes/parabolic_profile_coeffs_1", */
+  /*     "0.0 0.0 -$(1)/32.0 0.0 0.0 $(1)/8.0") ; */
 
-/*   // construct parameter object */
-/*   unsigned int dimension = 7; */
-/*   std::vector<std::shared_ptr<AGNOS::Parameter> > parameters; */
+  std::shared_ptr<PhysicsLibmesh<T_S,T_P> > grinsSolver ; 
+  grinsSolver = std::shared_ptr<PhysicsGrins<T_S,T_P> >(
+      new PhysicsGrins<T_S,T_P> ( comm,inputfile )
+      );
 
-/*   parameters.reserve(dimension); */
-/*   for(unsigned int i =0; i<dimension; i++) */
-/*     parameters.push_back( */ 
-/*         std::shared_ptr<AGNOS::Parameter>( */
-/*           new AGNOS::Parameter("CONSTANT",1.0, 1.0) ) */
-/*       ); */ 
-
-/*   // build the constantSurrogate model */
-/*   std::vector<unsigned int> order(dimension,0); */
-/*   order[0] = 4; */
-/*   order[4] = 4; */
-/*   std::set<std::string> computeSolutions ; */
-/*   computeSolutions.insert("primal"); */
-/*   computeSolutions.insert("adjoint"); */
-/*   /1* computeSolutions.insert("errorEstimate"); *1/ */
-/*   PseudoSpectralTensorProduct<T_S,T_P> constantSurrogate( */
-/*         comm, */
-/*         grinsSolver, */
-/*         parameters, */ 
-/*         order, */
-/*         computeSolutions */
-/*         ); */
-
-/*   constantSurrogate.build( ); */
-
-/*   // get the nominal solution we will compare to */
-/*   std::vector<double> nominalParamValue(dimension,1.); */
-/*   T_S paramValue(nominalParamValue); */
-/*   T_P primalSol = constantSurrogate.evaluate( "primal", paramValue ); */
-/*   T_P adjointSol = constantSurrogate.evaluate( "adjoint", paramValue ); */
+  // construct parameter object
+  unsigned int dimension = 2;
+  std::vector<std::shared_ptr<AGNOS::Parameter> > parameters;
 
 
-/*   // update parameters to small range around nominal values */ 
-/*   parameters.clear(); */
-/*   parameters.reserve(dimension); */
-/*   parameters.push_back( */ 
-/*       std::shared_ptr<AGNOS::Parameter>( */
-/*         new AGNOS::Parameter("UNIFORM",0.90, 1.05) ) */ 
-/*       ); */
-/*   for(unsigned int i =1; i<dimension; i++) */
-/*     parameters.push_back( */ 
-/*         std::shared_ptr<AGNOS::Parameter>( */
-/*           new AGNOS::Parameter("CONSTANT",1.00, 1.00) ) */
-/*       ); */ 
-/*   parameters[4] = std::shared_ptr<AGNOS::Parameter>( new AGNOS::Parameter("UNIFORM",0.90, 1.05) )  ; */
+  // build the constantSurrogate model
+  std::vector<unsigned int> order(dimension,2);
+  parameters.push_back( 
+      std::shared_ptr<AGNOS::Parameter>(
+        new AGNOS::Parameter("CONSTANT",0.055, 0.055) )
+    ); 
+  parameters.push_back( 
+      std::shared_ptr<AGNOS::Parameter>(
+        new AGNOS::Parameter("CONSTANT",6.0, 6.0) )
+    ); 
+
+  std::set<std::string> computeSolutions ;
+  computeSolutions.insert("primal");
+  computeSolutions.insert("adjoint");
+  computeSolutions.insert("errorEstimate");
+  PseudoSpectralTensorProduct<T_S,T_P> constantSurrogate(
+        comm,
+        grinsSolver,
+        parameters, 
+        order,
+        computeSolutions
+        );
+
+  constantSurrogate.build( );
+
+  // get the nominal solution we will compare to
+  std::vector<double> nominalParamValue(dimension,1.);
+  // neither of these is the mid point
+  nominalParamValue[0] = 0.05 ; 
+  nominalParamValue[1] = 5.00 ;
+  T_S paramValue(nominalParamValue);
+  T_P primalSol = constantSurrogate.evaluate( "primal", paramValue );
+  T_P adjointSol = constantSurrogate.evaluate( "adjoint", paramValue );
+
+
+  // update parameters to small range around nominal values 
+  parameters.clear();
+  parameters.reserve(dimension);
+  parameters.push_back( 
+      std::shared_ptr<AGNOS::Parameter>(
+        new AGNOS::Parameter("UNIFORM",0.01, 0.1) )
+    ); 
+  parameters.push_back( 
+      std::shared_ptr<AGNOS::Parameter>(
+        new AGNOS::Parameter("UNIFORM",3.0, 9.0) )
+    ); 
   
-/*   // build the uniformSurrogate model */
-/*   /1* std::vector<unsigned int> higherOrder(dimension,1); *1/ */
-/*   std::shared_ptr<AGNOS::PseudoSpectralTensorProduct<T_S,T_P> > */
-/*     uniformSurrogate( new PseudoSpectralTensorProduct<T_S,T_P>( */
-/*         comm, */
-/*         grinsSolver, */
-/*         parameters, */ 
-/*         order  ) */
-/*   ); */
+  // build the uniformSurrogate model
+  /* std::vector<unsigned int> higherOrder(dimension,1); */
+  std::shared_ptr<AGNOS::PseudoSpectralTensorProduct<T_S,T_P> >
+    uniformSurrogate( new PseudoSpectralTensorProduct<T_S,T_P>(
+        comm,
+        grinsSolver,
+        parameters, 
+        order  )
+  );
 
-/*   // build a secondary surrogate just to test errorEstimate */
-/*   std::vector<unsigned int> increaseOrder(dimension,0); */
-/*   unsigned int multiplyOrder(2); */
-/*   std::set<std::string> secondarySolutions ; */
-/*   secondarySolutions.insert("errorEstimate"); */
-/*   std::shared_ptr<AGNOS::PseudoSpectralTensorProduct<T_S,T_P> > */
-/*   secondarySurrogate( new PseudoSpectralTensorProduct<T_S,T_P>( */
-/*       uniformSurrogate, */
-/*       increaseOrder, */
-/*       multiplyOrder, */
-/*       computeSolutions, */
-/*       secondarySolutions ) */
-/*       ); */
+  // build a secondary surrogate just to test errorEstimate
+  std::vector<unsigned int> increaseOrder(dimension,1);
+  unsigned int multiplyOrder(1);
+  std::set<std::string> secondarySolutions ;
+  secondarySolutions.insert("errorEstimate");
+  std::shared_ptr<AGNOS::PseudoSpectralTensorProduct<T_S,T_P> >
+  secondarySurrogate( new PseudoSpectralTensorProduct<T_S,T_P>(
+      uniformSurrogate,
+      increaseOrder,
+      multiplyOrder,
+      computeSolutions,
+      secondarySolutions )
+      );
 
-/*   T_P primalPred, adjointPred; */
-/*   double surrogateError = 0.; */
-/*   int iter=0, maxIter=4; */
-/*   double tol = 1e-3, primalDiff,adjointDiff; */
-/*   bool condition = true ; */
-/*   do */ 
-/*   { */
-/*     iter++; */
+  T_P primalPred, adjointPred;
+  double surrogateError = 0.;
+  int iter=0, maxIter=2;
+  double tol = 1e-3, primalDiff,adjointDiff;
+  bool condition = true ;
+  do 
+  {
+    iter++;
 
-/*     uniformSurrogate->refine( ); */
-/*     secondarySurrogate->refine(); */
-/*     uniformSurrogate->build( ); */
-/*     secondarySurrogate->build( ); */
+    uniformSurrogate->refine( );
+    secondarySurrogate->refine();
+    uniformSurrogate->build( );
+    secondarySurrogate->build( );
 
-/*     primalPred = uniformSurrogate->evaluate( "primal", paramValue ); */
-/*     primalPred -= primalSol ; */
-/*     primalDiff = primalPred.linfty_norm() ; */
-/*     std::cout << "primalDiff = " << primalDiff << std::endl; */
+    primalPred = uniformSurrogate->evaluate( "primal", paramValue );
+    primalPred -= primalSol ;
+    primalDiff = primalPred.linfty_norm() ;
+    std::cout << "primalDiff = " << primalDiff << std::endl;
 
-/*     adjointPred = uniformSurrogate->evaluate( "adjoint", paramValue ); */
-/*     adjointPred -= adjointSol ; */
-/*     adjointDiff = adjointPred.linfty_norm() ; */
-/*     std::cout << "adjointDiff = " << adjointDiff << std::endl; */
+    adjointPred = uniformSurrogate->evaluate( "adjoint", paramValue );
+    adjointPred -= adjointSol ;
+    adjointDiff = adjointPred.linfty_norm() ;
+    std::cout << "adjointDiff = " << adjointDiff << std::endl;
 
-/*     surrogateError = secondarySurrogate->l2Norm("errorEstimate")(0); */
-/*     std::cout << "surrogateError = " << surrogateError << std::endl; */
+    surrogateError = uniformSurrogate->l2NormDifference(
+        *secondarySurrogate,
+        "errorEstimate");
+    std::cout << "physicalError = " <<
+      uniformSurrogate->l2Norm("errorEstimate")(0) << std::endl;
+    std::cout << "surrogateError = " << surrogateError << std::endl;
+    std::cout << "totalError = " <<
+      secondarySurrogate->l2Norm("errorEstimate")(0) << std::endl;
 
-/*     std::cout << " iter<maxIter: " << (iter<maxIter) << std::endl; */
-/*     std::cout << " primalDiff > tol: " << (primalDiff>tol) << std::endl; */
-/*     std::cout << " adjointDiff > tol: " << (adjointDiff>tol) << std::endl; */
-/*     std::cout << " surrogateError > tol: " << (surrogateError>tol) << std::endl; */
-/*     condition = */ 
-/*        (iter<maxIter) && ( */
-/*        (primalDiff > tol) || (adjointDiff > tol) || (surrogateError > tol) */ 
-/*        ); */
-/*     std::cout << "condition: " << condition << std::endl; */
+    std::cout << " iter<maxIter: " << (iter<maxIter) << std::endl;
+    std::cout << " primalDiff > tol: " << (primalDiff>tol) << std::endl;
+    std::cout << " adjointDiff > tol: " << (adjointDiff>tol) << std::endl;
+    std::cout << " surrogateError > tol: " << (surrogateError>tol) << std::endl;
+    condition = 
+       (iter<maxIter) && (
+       (primalDiff > tol) || (adjointDiff > tol) || (surrogateError > tol) 
+       );
+    std::cout << "condition: " << condition << std::endl;
 
-/*   } while ( condition ) ; */
+  } while ( condition ) ;
 
-/*   // for 2 iterations diff should be within 1e-3 */
-/*   // for 3 its within 1e-6 (this takes a very long time to run though) */
-/*   BOOST_REQUIRE_SMALL( primalDiff, tol  ); */
-/*   BOOST_REQUIRE_SMALL( adjointDiff, tol  ); */
-/*   BOOST_REQUIRE_SMALL( surrogateError, tol  ); */
+  // for 2 iterations diff should be within 1e-3
+  // for 3 its within 1e-6 (this takes a very long time to run though)
+  BOOST_REQUIRE_SMALL( primalDiff, tol  );
+  BOOST_REQUIRE_SMALL( adjointDiff, tol  );
+  BOOST_REQUIRE_SMALL( surrogateError, tol  );
 
-/* } */
+}
