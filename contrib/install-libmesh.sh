25c2c6479f5236fd891943134958e4012cf2fb7f
Corey M Bryant <corey.m.bryant@gmail.com>
2016-06-29 14:01:19 -0500

    [build] Delete install-matlab.sh
    opting for self-contained travis.yml file

--- contrib/install-libmesh.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -ex
-wget https://github.com/libMesh/libmesh/releases/download/v1.0.0/libmesh-1.0.0.tar.gz
-tar -xzvf libmesh-1.0.0.tar.gz
-cd libmesh-1.0.0/ && ./configure --prefix=/usr && make && sudo make install
