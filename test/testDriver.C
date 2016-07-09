259c2f9870850861c448518d275e11df35dc96e9
Corey M Bryant <corey.m.bryant@gmail.com>
2016-07-09 08:57:32 -0500

    [build] reworking cppunit test driver

--- test/testDriver.C
+++ test/testDriver.C
@@ -8,25 +8,26 @@ using namespace libMesh;
 
 int main( int argc, char **argv)
 {
-  MPI_Init(&argc,&argv);
-  Communicator comm(MPI_COMM_WORLD);
+  /* MPI_Init(&argc,&argv); */
+  /* Communicator comm(MPI_COMM_WORLD); */
 
-  {
-    MPI_Comm subComm;
-    int mpiSplit =  
-      MPI_Comm_split( MPI_COMM_WORLD, comm.rank(), 0, &subComm);
-    LibMeshInit libmesh_init(argc, argv, subComm);
+  /* { */
+    /* MPI_Comm subComm; */
+    /* int mpiSplit = */  
+      /* MPI_Comm_split( MPI_COMM_WORLD, comm.rank(), 0, &subComm); */
+    /* LibMeshInit libmesh_init(argc, argv, subComm); */
+    LibMeshInit libmesh_init(argc, argv);
 
     CppUnit::TextUi::TestRunner runner;
     CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
     runner.addTest( registry.makeTest() );
     runner.run();
-  }
+  /* } */
 
-#ifndef AGNOS_USING_MPICH
-  int ierr;
-  comm.barrier();
-  /* MPI_Finalize(); */
-#endif
+/* #ifndef AGNOS_USING_MPICH */
+/*   int ierr; */
+/*   comm.barrier(); */
+/*   /1* MPI_Finalize(); *1/ */
+/* #endif */
   return 0;
 }
